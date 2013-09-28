c4672959bf746d0c3c5126273f6f00a1de456f83
cslgxuchang <ITxuchang@163.com>
2013-09-28 22:10:52 +0800

    web flickr
    do the work on the page of the flickr

--- imgfave/js/index.js
+++ imgfave/js/index.js
@@ -5,70 +5,35 @@ $(function(){
 
  
 })
-// var i=0;
-//  function add(){
-//  	$(".select").css("background-color","#ccc");
-//   i++;
-//   if(i!=6){
-//   	$("#cimg").css("background-image",'url("css/image/'+i+'.jpg")');
-//   	$(".select").each(function(){
-//   		if (i==$(this).attr("txt")){
-//   			$(this).css("background-color","#f08200");
-//   		};
 
-//   	})
-//   }else{
-//   	i=1;
-//   	$("#cimg").css("background-image",'url("css/image/'+i+'.jpg")');
-//   	$(".select").first().css("background-color","#f08200");
-//   }
-  
- 	
- }
+
 
 function  events(){
-	// $(window).scroll(function(){ 
+	$("#titlea").hover(function(){
+		
+		$("#topdiv1").show();
+		$("#topdiv1").css("background-color","#fff");
+		$("#titlea a").css("color","#ff0084");
+	},function(){
 
-	// 	if($(this).scrollTop()>72){
+		$("#topdiv1").hide();
+		$("#titlea a").css("color","#fff");
+	});
 
-	// 	$("#headtd").css("top",($(this).scrollTop()) + "px");
-
-	// 	} else{
-	// 		$("#headtd").css("top","72px");
-	// 	}
-	// } );
- //   $(window).scroll(function(){ 
-
-	// 	if($(this).scrollTop()>130){
-
-	// 	$("#back").show();
-	// 	$("#back").css("top",($(this).scrollTop()-40) + "px")
-
-	// 	} else{
-	// 		$("#back").hide();
-	// 		// $("#back").css("position","absolute")
-	// 	}
-	// } );
-
-
- //   $("#backa").click(function(){
- //     $("html,body").animate({scrollTop:0},2000);
- //   });
+	$("#topdiv1 li").hover(function(){
+		$("#topdiv1").show();
+		$("#topdiv1").css("background-color","#fff");
+		$(this).find("a").css("color","#0063dc");
+		$(this).css("background-color","#f1f1f1");
+		$("#titlea a").css("color","#ff0084");
+	},function(){
+		$("#topdiv1").hide();
+		$("#titlea a").css("color","#fff");
+		$(this).find("a").css("color","#373737");
+		$(this).css("background-color","#fff");
+	});
 
 
-	// $("#tubiao").hover(function(){
-	// 	$("#over").show();
-	// },function(){
-	// 	$("#over").hide();
-	// })
 
-
-	// $(".select").click(function(){
-	// 	var curindex=$(this).attr("txt");
-	// 	$(".select").css("background-color","#ccc");
-	// 	$("#cimg").css("background-image",'url("css/image/'+curindex+'.jpg")');
-	// 	$(this).css("background-color","#f08200");
-
-	// })
   
 }
